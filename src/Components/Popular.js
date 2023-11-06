54dc6d33c0e116372d044410f623f4f2dbeacd52
Skwafaaaks <eacedo4@gmail.com>
2023-11-06 15:04:49 +0800

    Mp2 Final

---
+++ src/Components/Popular.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useGlobalContext } from '../context/global';
+import styled from 'styled-components';
+import Sidebar from './Sidebar';
+
+function Popular({ rendered }) {
+  const { popularAnime, isSearch, searchResults } = useGlobalContext();
+
+  const conditionalRender = () => {
+    if (!isSearch && rendered === 'popular') {
+      return popularAnime?.map((anime) => {
+        return (
+          <Link
+            to={`/anime/${anime.mal_id}`}
+            key={anime.mal_id}
+          >
+            <img
+              src={anime.images.jpg.large_image_url}
+              alt=""
+            />
+            <h4>{anime.title}</h4>
+          </Link>
+        );
+      });
+    } else {
+      return searchResults?.map((anime) => {
+        return (
+          <Link
+            to={`/anime/${anime.mal_id}`}
+            key={anime.mal_id}
+          >
+            <img
+              src={anime.images.jpg.large_image_url}
+              alt=""
+            />
+            <h3>{anime.title}</h3>
+          </Link>
+        );
+      });
+    }
+  };
+
+  return (
+    <PopularStyled>
+      <div className="popular-anime">{conditionalRender()}</div>
+      <Sidebar />
+    </PopularStyled>
+  );
+}
+
+const PopularStyled = styled.div`
+  * {
+    background-color: #0f0f0f;
+  }
+  display: flex;
+  h3 {
+    padding: 10px;
+    border: 10px;
+    color: #9bbec8;
+  }
+  h4,
+  h3 {
+    color: #f8c119;
+  }
+
+  .popular-anime {
+    margin-top: 2rem;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+    padding-left: 5rem;
+    padding-right: 0;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-gap: 2rem;
+    background-color: #0f0f0f;
+    border-top: 5px solid #f8c119;
+    a {
+      height: 500px;
+      border-radius: 7px;
+      border: 5px solid #f8c119;
+    }
+    a img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      border-radius: 5px;
+    }
+  }
+`;
+
+export default Popular;
---
+++ src/Components/Upcoming.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useGlobalContext } from '../context/global';
+import styled from 'styled-components';
+import Sidebar from './Sidebar';
+
+function Upcoming({ rendered }) {
+  const { upcomingAnime, isSearch, searchResults } = useGlobalContext();
+
+  const conditionalRender = () => {
+    if (!isSearch && rendered === 'upcoming') {
+      return upcomingAnime?.map((anime) => {
+        return (
+          <Link
+            to={`/anime/${anime.mal_id}`}
+            key={anime.mal_id}
+          >
+            <img
+              src={anime.images.jpg.large_image_url}
+              alt=""
+            />
+            <h4>{anime.title}</h4>
+          </Link>
+        );
+      });
+    } else {
+      return searchResults?.map((anime) => {
+        return (
+          <Link
+            to={`/anime/${anime.mal_id}`}
+            key={anime.mal_id}
+          >
+            <img
+              src={anime.images.jpg.large_image_url}
+              alt=""
+            />
+            <h3>{anime.title}</h3>
+          </Link>
+        );
+      });
+    }
+  };
+
+  return (
+    <PopularStyled>
+      <div className="upcoming-anime">{conditionalRender()}</div>
+      <Sidebar />
+    </PopularStyled>
+  );
+}
+
+const PopularStyled = styled.div`
+  display: flex;
+  h4,
+  h3 {
+    color: #f8c119;
+  }
+  .upcoming-anime {
+    margin-top: 2rem;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+    padding-left: 5rem;
+    padding-right: 0;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-gap: 2rem;
+    background-color: #0f0f0f;
+    border-top: 5px solid #f8c119;
+    a {
+      height: 500px;
+      border-radius: 7px;
+      border: 5px solid #f8c119;
+    }
+    a img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      border-radius: 5px;
+    }
+  }
+`;
+
+export default Upcoming;
